f8e03cb7d91bff0a41be2280b1514af16c1d91c6
StillARockstar <m.schoder@my-realm.net>
2020-10-22 18:06:42 +0200

    setup project

---
+++ WristEmojis WatchKit Extension/WristEmojisApp.swift
@@ -0,0 +1,19 @@
+//
+//  WristEmojisApp.swift
+//  WristEmojis WatchKit Extension
+//
+//  Created by Michael Schoder on 22.10.20.
+//
+
+import SwiftUI
+
+@main
+struct WristEmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
