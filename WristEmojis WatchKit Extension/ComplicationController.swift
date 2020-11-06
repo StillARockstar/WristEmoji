70b344cdfaf6fa528c2252fa4ac6437579aa4b5a
StillARockstar <m.schoder@my-realm.net>
2020-11-06 22:45:35 +0100

    load complications

--- WristEmojis WatchKit Extension/ComplicationController.swift
+++ WristEmojis WatchKit Extension/ComplicationController.swift
@@ -9,15 +9,24 @@ import ClockKit
 
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
+    var dataProvider: DataProvider { AppDataProvider() }
     
-    // MARK: - Complication Configuration
+    // MARK: Complication Configuration
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
-        let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "WristEmojis", supportedFamilies: CLKComplicationFamily.allCases)
-            // Multiple complication support can be added here with more descriptors
-        ]
-        
+        var descriptors = [CLKComplicationDescriptor]()
+
+        for configuration in self.dataProvider.userData.configurations {
+            descriptors.append(
+                CLKComplicationDescriptor(
+                    identifier: "\(configuration.id)_\(Date().timeIntervalSince1970)",
+                    displayName: configuration.name,
+                    supportedFamilies: [.modularSmall, .modularLarge, .utilitarianSmall, .utilitarianSmallFlat, .utilitarianLarge, .circularSmall, .extraLarge, .graphicCircular, .graphicCorner],
+                    userInfo: ["id": configuration.id]
+                )
+            )
+        }
+
         // Call the handler with the currently supported complication descriptors
         handler(descriptors)
     }
@@ -26,34 +35,90 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         // Do any necessary work to support these newly shared complication descriptors
     }
 
-    // MARK: - Timeline Configuration
-    
-    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        // Call the handler with the last entry date you can currently provide or nil if you can't support future timelines
-        handler(nil)
-    }
+    // MARK: Timeline Configuration
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
         // Call the handler with your desired behavior when the device is locked
         handler(.showOnLockScreen)
     }
 
-    // MARK: - Timeline Population
+    // MARK: Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after the given date
-        handler(nil)
+        guard let id = complication.userInfo?["id"] as? String,
+              let emoji = self.dataProvider.userData.configurations.first(where: { $0.id == id })?.emoji,
+              let template = self.template(for: complication.family, emoji: emoji) else {
+            handler(nil)
+            return
+        }
+        handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
     }
 
-    // MARK: - Sample Templates
+    // MARK: Sample Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        guard let id = complication.userInfo?["id"] as? String,
+              let emoji = self.dataProvider.userData.configurations.first(where: { $0.id == id })?.emoji else {
+            handler(nil)
+            return
+        }
+        handler(self.template(for: complication.family, emoji: emoji))
     }
+
+    // MARK: Helper Methods
+
+    private func template(for family: CLKComplicationFamily, emoji: String) -> CLKComplicationTemplate? {
+        switch family {
+        case .modularSmall:
+            return CLKComplicationTemplateModularSmallSimpleText(textProvider: CLKTextProvider(format: emoji))
+        case .modularLarge:
+            return CLKComplicationTemplateModularLargeTallBody(headerTextProvider: CLKTextProvider(format: ""), bodyTextProvider: CLKTextProvider(format: emoji))
+        case .utilitarianSmall:
+            return CLKComplicationTemplateUtilitarianSmallFlat(textProvider: CLKTextProvider(format: emoji))
+        case .utilitarianSmallFlat:
+            return CLKComplicationTemplateUtilitarianSmallFlat(textProvider: CLKTextProvider(format: emoji))
+        case .utilitarianLarge:
+            return CLKComplicationTemplateUtilitarianLargeFlat(textProvider: CLKTextProvider(format: emoji))
+        case .circularSmall:
+            return CLKComplicationTemplateCircularSmallSimpleText(textProvider: CLKTextProvider(format: emoji))
+        case .extraLarge:
+            return CLKComplicationTemplateExtraLargeSimpleText(textProvider: CLKTextProvider(format: emoji))
+        case .graphicCircular:
+            guard let image = emoji.image(size: CGSize(width: 47, height: 47)) else {
+                return nil
+            }
+            return CLKComplicationTemplateGraphicCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: image))
+        case .graphicCorner:
+            guard let image = emoji.image(size: CGSize(width: 36, height: 36)) else {
+                return nil
+            }
+            return CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: image))
+        case .graphicBezel, .graphicRectangular, .graphicExtraLarge:
+            return nil
+        @unknown default:
+            return nil
+        }
+    }
+}
+
+private extension String {
+
+    func image(size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, true, 2.0)
+
+        let style = NSMutableParagraphStyle()
+        style.alignment = NSTextAlignment.center
+
+        (self as NSString).draw(
+            in: CGRect(origin: CGPoint(x: 0, y: size.height / 5), size: size),
+            withAttributes: [
+                .font: UIFont.systemFont(ofSize: size.height / 2.0),
+                .paragraphStyle: style
+            ]
+        )
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
 }
--- WristEmojis WatchKit Extension/WristEmojisApp.swift
+++ WristEmojis WatchKit Extension/WristEmojisApp.swift
@@ -6,14 +6,34 @@
 //
 
 import SwiftUI
+import Combine
+import ClockKit
 
 @main
 struct WristEmojisApp: App {
+    @WKExtensionDelegateAdaptor(ExtensionDelegate.self) var extensionDelegate
+
     var body: some Scene {
         WindowGroup {
             HomeView()
-                .environmentObject(HomeViewProvider(dataProvider: AppDataProvider()))
+                .environmentObject(HomeViewProvider(dataProvider: extensionDelegate.dataProvider))
                 .embedInNavigation()
+        }
+    }
+}
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+    private let configurationsPublisher: AnyCancellable
+    let dataProvider: DataProvider
+
+    override init() {
+        self.dataProvider = AppDataProvider()
+
+        self.configurationsPublisher = dataProvider.userData.configurationsPublisher.sink { newValue in
+            CLKComplicationServer.sharedInstance().activeComplications?.forEach {
+                CLKComplicationServer.sharedInstance().reloadTimeline(for: $0)
+            }
+            CLKComplicationServer.sharedInstance().reloadComplicationDescriptors()
         }
     }
 }
