43e11d67cae723d11e0fa9b33b0a0a4399d38d35
StillARockstar <m.schoder@my-realm.net>
2020-11-08 13:15:00 +0100

    generate emoji version lists

---
+++ EmojiCurator.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+import PlaygroundSupport
+
+
+
+enum CuratorError: Error {
+    case noFileContent
+    case encodingFailed
+    case urlOutputFailed
+}
+
+struct EmojiCategory: Encodable {
+    let name: String
+    let emojis: [String]
+}
+
+
+
+let filePath = Bundle.main.path(forResource:"emojis_14_2", ofType: "txt")
+let contentData = FileManager.default.contents(atPath: filePath!)
+let content = String(data:contentData!, encoding:String.Encoding.utf8)
+
+
+
+print("Processing: \(filePath!)")
+
+guard let content = content else {
+    print("Failed loading content")
+    throw CuratorError.noFileContent
+}
+let splitContent = content.split(separator: "\n")
+print("Found \(splitContent.count) lines")
+
+
+
+var categories = [EmojiCategory]()
+
+for (index, singleContent) in splitContent.enumerated() {
+    if singleContent.starts(with: "-") {
+        let categoryName = singleContent.replacingOccurrences(of: "-", with: "")
+        let categoryEmojis = Array(splitContent[index + 1]).map({ String($0) })
+
+        let category = EmojiCategory(
+            name: categoryName,
+            emojis: categoryEmojis
+        )
+        print("Built category: \(category.name) with emojis \(category.emojis.count)")
+        categories.append(category)
+    }
+}
+
+print("Built \(categories.count) categories")
+
+
+
+let encoder = JSONEncoder()
+guard let outputData = try? encoder.encode(categories) else {
+    throw CuratorError.encodingFailed
+}
+print(String(data: outputData, encoding: .utf8) ?? "No output")
