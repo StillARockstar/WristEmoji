45966c1dcb4eb4811a8bfb7ff3506c5fafd1b0d5
StillARockstar <m.schoder@my-realm.net>
2020-12-08 16:32:31 +0100

    move EmojiConfiguration to own file

--- WristEmojis WatchKit Extension/HomeView/HomeViewProvider.swift
+++ WristEmojis WatchKit Extension/HomeView/HomeViewProvider.swift
@@ -8,32 +8,6 @@
 import Foundation
 import Combine
 
-typealias EmojiString = String
-
-struct EmojiConfiguration: Codable, Equatable {
-    let id: String
-    let emoji: EmojiString
-    let name: String
-
-    func encode() -> String? {
-        guard let jsonData = try? JSONEncoder().encode(self) else {
-            return nil
-        }
-        return String(data: jsonData, encoding: .utf8)
-    }
-
-    static func from(_ dataString: String) -> Self? {
-        guard let data = dataString.data(using: .utf8) else {
-            return nil
-        }
-        return try? JSONDecoder().decode(Self.self, from: data)
-    }
-
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.id == rhs.id
-    }
-}
-
 class HomeViewProvider: ObservableObject {
     let dataProvider: DataProvider
     private var subscriptions: Set<AnyCancellable> = Set()
