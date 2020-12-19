487adf002b090afb1ac8ddc917fc029385697b0b
StillARockstar <m.schoder@my-realm.net>
2020-12-19 18:01:50 +0100

    add InfoView

---
+++ WristEmojis WatchKit Extension/Info/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  WristEmojis WatchKit Extension
+//
+//  Created by Michael Schoder on 19.12.20.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
