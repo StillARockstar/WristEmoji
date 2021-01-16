e6cad70e39eb107f2890e902c23be140dd4d8a6a
Michael Schoder <m.schoder@my-realm.net>
2021-01-16 20:04:56 +0100

    finalize skincolor export

--- EmojiCurator.playground/Sources/SkintoneModifier.swift
+++ EmojiCurator.playground/Sources/SkintoneModifier.swift
@@ -3,16 +3,69 @@ import UIKit
 
 public extension String {
     private static let emojiSkinToneModifiers: [String] =  ["🏻", "🏼", "🏽", "🏾", "🏿"]
+    private static let hardcodedSkinToneEmojis: [String: [String]] = [
+        "🕵️‍♀️": ["🕵️‍♀️", "🕵🏻‍♀️", "🕵🏼‍♀️", "🕵🏽‍♀️", "🕵🏾‍♀️", "🕵🏿‍♀️"],
+        "🕵️‍♂️": ["🕵️‍♂️", "🕵🏻‍♂️", "🕵🏼‍♂️", "🕵🏽‍♂️", "🕵🏾‍♂️", "🕵🏿‍♂️"],
+        "🏋️‍♀️": ["🏋️‍♀️", "🏋🏻‍♀️", "🏋🏼‍♀️", "🏋🏽‍♀️", "🏋🏾‍♀️", "🏋🏿‍♀️"],
+        "🏋️‍♂️": ["🏋️‍♂️", "🏋🏻‍♂️", "🏋🏼‍♂️", "🏋🏽‍♂️", "🏋🏾‍♂️", "🏋🏿‍♂️"],
+        "⛹️‍♀️": ["⛹️‍♀️", "⛹🏻‍♀️", "⛹🏼‍♀️", "⛹🏽‍♀️", "⛹🏾‍♀️", "⛹🏿‍♀️"],
+        "⛹️‍♂️": ["⛹️‍♂️", "⛹🏻‍♂️", "⛹🏼‍♂️", "⛹🏽‍♂️", "⛹🏾‍♂️", "⛹🏿‍♂️"],
+        "🏌️‍♀️": ["🏌️‍♀️", "🏌🏻‍♀️", "🏌🏼‍♀️", "🏌🏽‍♀️", "🏌🏾‍♀️", "🏌🏿‍♀️"],
+        "🏌️‍♂️": ["🏌️‍♂️", "🏌🏻‍♂️", "🏌🏼‍♂️", "🏌🏽‍♂️", "🏌🏾‍♂️", "🏌🏿‍♂️"]
+    ]
 
-    private var emojiUnmodified: String {
-        if isEmpty {
-            return self
+    var allSkinToneModifiedEmojis: [String] {
+        guard self.canHaveSkinToneModifier else {
+            return []
         }
-        let string = String(self.unicodeScalars.first!)
-        return string
+        if let hardcodedSkinToneEmojis = Self.hardcodedSkinToneEmojis[self] {
+            print("failed applying on \(self) ==> Using hardcoded emojis")
+            return hardcodedSkinToneEmojis
+        }
+
+        var emojis = [String]()
+        emojis.append(self)
+        for modifier in Self.emojiSkinToneModifiers {
+            guard let modifiedEmoji = self.apply(modifier: modifier) else {
+                print("failed applying on \(self) ==> Unhandled emoji variation")
+                return []
+            }
+            emojis.append(modifiedEmoji)
+        }
+
+        return emojis
     }
 
-    private var glyphSize: CGSize {
+    private var canHaveSkinToneModifier: Bool {
+        guard !isEmpty else {
+            return false
+        }
+
+        let modified = self.apply(modifier: Self.emojiSkinToneModifiers.first!) ?? ""
+        return self != modified
+    }
+
+    private func apply(modifier: String) -> String? {
+        var result = ""
+        for scalar in self.unicodeScalars {
+            if scalar.properties.isEmojiModifierBase {
+                result.append("\(scalar)" + modifier)
+            } else {
+                result.append("\(scalar)")
+            }
+        }
+
+        let charResult = String(Character(result))
+        if self.glyphSize != charResult.glyphSize {
+            return nil
+        }
+
+        return String(Character(result))
+    }
+}
+
+private extension String {
+    var glyphSize: CGSize {
         return (self as NSString)
             .boundingRect(
                 with: CGSize(
@@ -23,25 +76,7 @@ public extension String {
                 context: nil)
             .size
     }
-
-    private var canHaveSkinToneModifier: Bool {
-        guard !isEmpty else {
-            return false
-        }
-
-        let modified = self.emojiUnmodified + Self.emojiSkinToneModifiers[0]
-        return self.emojiUnmodified.glyphSize == modified.glyphSize
-    }
-
-    var allSkinToneModifiedEmojis: [String]? {
-        guard self.canHaveSkinToneModifier else {
-            return nil
-        }
-        var emojis = [String]()
-        emojis.append(self)
-        for modifier in Self.emojiSkinToneModifiers {
-            emojis.append(self.emojiUnmodified + modifier)
-        }
-        return emojis
-    }
 }
+
+
+
